6de1731ad7b6846be47358f5cd8e9ccf5a9395fd
Benjamin Pontes <45019346+Benjaminxciv@users.noreply.github.com>
2019-09-29 13:22:43 -0500

    Add files via upload

--- testingconsole.cpp
+++ testingconsole.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
 int main(){
 
-int height = 15;
-int width = 20;
+    int height = 750;
+    int width = 1000;
 
-cout << "     " ;
-for(int i = 1; i < width+1; i++)
-{
-    cout.fill('0');
-    cout.width(3);
-    cout << i << " ";
-}
+    std::string column_space =  "     ";
+    std::cout << column_space;
+
+    for(int i = 1; i < (width/25)+1; i++)
+    {
+        cout.fill('0');
+        cout.width(3);
+        cout << i*25 << " ";
+    }
     cout << endl;
 
-for (int j = 0; j < height+1; j++)
+    for (int j = 0; j < (height/25)+1; j++)
     {   
         cout.fill('0');
         cout.width(3);
-        cout << j;
+        cout << j*25;
 
-        for(int k = 0; k < width;k++)
+        for(int k = 0; k < (width/25);k++)
         {
             cout.fill(' ');
             cout.width(4);
-           get cout << "*";
+            cout << "*";
         }
-        cout << endl;
+            cout << endl;
     }
-
-
     return 0;
 }
